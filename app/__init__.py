05507eb86c26951e24e2a02426a1f0be432f2bff
Carlos Seda <carlosseda123@gmail.com>
2022-06-02 12:48:12 -0500

    Moving the files

--- app/__init__.py
+++ app/__init__.py
@@ -1,11 +1,9 @@
-import os
-from flask import Flask, render_template, request
-from dotenv import load_dotenv
+from flask import Flask
+from views import views
 
-load_dotenv()
 app = Flask(__name__)
 
+app.register_blueprint(views, url_prefix="/")
 
-@app.route('/')
-def index():
-    return render_template('index.html', title="MLH Fellow", url=os.getenv("URL"))
+if __name__ == "__main__":
+    app.run(debug=True)
