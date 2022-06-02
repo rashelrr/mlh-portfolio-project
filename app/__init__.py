ac23e4da1ed231e03e45436e87b0bd835d04d24a
Rashel Rojas <rdr2139@columbia.edu>
2022-06-02 19:30:58 -0400

    fixed some bugs

--- app/__init__.py
+++ app/__init__.py
@@ -1,9 +1,5 @@
 from flask import Flask
-from views import views
+from app.views import views
 
 app = Flask(__name__)
-
-app.register_blueprint(views, url_prefix="/")
-
-if __name__ == "__main__":
-    app.run(debug=True)
+app.register_blueprint(views)
--- app/views.py
+++ app/views.py
@@ -1,22 +1,10 @@
+import os
 from flask import Blueprint, render_template
+from dotenv import load_dotenv
 
-views = Blueprint(__name__, "views")
+load_dotenv()
+views = Blueprint('views', __name__, template_folder='templates')
 
 @views.route("/")
-def home():
-    username = "Carlos Seda"
-    about_me = '''I am a computer science student who 
-                faithfully believes that technology is 
-                very powerful and is important for 
-                evolution. Developing, innovating and learning 
-                is what I do daily. I like to be aware of new 
-                technologies and everything new that technology 
-                companies create every day.'''
-
-    return render_template("index.html", name = username, about = about_me)
-
-@views.route("hobbies")
-def hobbies():
-    return render_template("index.html")
-
-
+def index():
+    return render_template('index.html', url=os.getenv("URL"))
