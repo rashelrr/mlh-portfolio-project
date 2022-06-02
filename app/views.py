a397abbe07cbf567f0c14eaf5d36c99529d17eca
Carlos Seda <carlosseda123@gmail.com>
2022-06-02 15:34:03 -0500

    Only important files

--- app/views.py
+++ app/views.py
@@ -4,16 +4,7 @@ views = Blueprint(__name__, "views")
 
 @views.route("/")
 def home():
-    username = "Carlos Seda"
-    about_me = '''I am a computer science student who 
-                faithfully believes that technology is 
-                very powerful and is important for 
-                evolution. Developing, innovating and learning 
-                is what I do daily. I like to be aware of new 
-                technologies and everything new that technology 
-                companies create every day.'''
-
-    return render_template("index.html", name = username, about = about_me)
+    return render_template("index.html")
 
 @views.route("hobbies")
 def hobbies():
