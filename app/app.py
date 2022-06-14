c10427350fdc0d86f081f2955c34aeb54ae459be
Rashel Rojas <rdr2139@columbia.edu>
2022-06-14 18:02:14 -0400

    implemented loading homepage with new UI design

--- app/app.py
+++ app/app.py
@@ -11,30 +11,17 @@ data = json.loads(f.read())
 
 @app.route("/")
 def index():
-    return render_template('index.html', name = data['name'], 
+    return render_template('test.html', name = data['name'], 
                         university = data['university'], 
                         about = data['about_yourself'], 
-                        about2 = data['about_yourself2'],
                         education = data['education'], 
-                        education_names = data['education_names'],
-                        education_gpa = data['education_gpa'],
-                        education_grad = data['education_graduation'],
-                        education_duration = data['education_duration'],
-                        job_titles = data['job_titles'],
-                        company_names = data['company_names'],
-                        start_dates = data['start_dates'],
-                        end_dates = data['end_dates'],
-                        locations = data['locations'],
-                        responsibilities = data['responsibilities'],
+                        experiences = data['experiences'],
                         url=os.getenv("URL"))
 
 @app.route("/hobbies")
 def hobbies():
     return render_template('hobbies.html',
-                        name = data['name'],
-                        images = data['images'],
                         hobbies = data['hobbies'],
-                        descriptions = data['descriptions'],
                         url=os.getenv("URL"))
 
 @app.route("/map")
