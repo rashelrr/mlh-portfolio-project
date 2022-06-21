77c9094bb3419d02abf0d4c30d6d3c26f072311f
Rashel Rojas <rdr2139@columbia.edu>
2022-06-21 13:50:13 -0400

    finished hobbies

---
+++ app/static/scripts/sidebar.js
@@ -0,0 +1,9 @@
+function openNav() {
+    document.getElementById("mySidenav").style.width = "auto";
+    document.getElementById("mySidenav").style.border = "1px solid #000000;";
+}
+
+function closeNav() {
+    document.getElementById("mySidenav").style.width = "0";
+    document.getElementById("mySidenav").style.border = "0";
+}
