5b314da64de0ac76d980a7f36443089412d61166
Rashel Rojas <66976912+rashelrr@users.noreply.github.com>
2022-06-22 14:27:54 -0400

    Added connection to mysql database in init file

--- app/__init__.py
+++ app/__init__.py
@@ -1,5 +1,15 @@
+from peewee import *
 from flask import Flask
 from app.app import app
 
 x = Flask(__name__)
 x.register_blueprint(app)
+
+mydb = MySQLDatabase(os.getenv("MYSQL_DATABASE"),
+                     user=os.getenv("MYSQL_USER"),
+                     password=os.getenv("MYSQL_PASSWORD"),
+                     host=os.getenv("MYSQL_HOST"),
+                     port=3306
+                    )
+
+print(mydb)
