ad03dc6ee7bcef2b865c99dfff83c519c786aca3
Rashel Rojas <rdr2139@columbia.edu>
2022-06-22 15:20:57 -0400

    Added timeline get, post to init

--- app/__init__.py
+++ app/__init__.py
@@ -1,7 +1,9 @@
 from peewee import *
-from flask import Flask
+from playhouse.shortcuts import model_to_dict
+from flask import Flask, request
 from app.app import app
 import os
+import datetime
 
 x = Flask(__name__)
 x.register_blueprint(app)
@@ -13,4 +15,34 @@ mydb = MySQLDatabase(os.getenv("MYSQL_DATABASE"),
                      port=3306
                     )
 
+class TimelinePost(Model):
+    name = CharField()
+    email = CharField()
+    content = TextField()
+    created_at = DateTimeField(default=datetime.datetime.now)
+
+    class Meta:
+        database = mydb
+
+mydb.connect()
+mydb.create_tables([TimelinePost])
+
 print(mydb)
+
+@app.route('/api/timeline_post', methods=['POST'])
+def post_time_line_post():
+    name = request.form['name']
+    email = request.form['email']
+    content = request.form['content']
+    timeline_post = TimelinePost.create(name=name, email=email, content=content)
+
+    return model_to_dict(timeline_post)
+
+@app.route('/api/timeline_post', methods=['GET'])
+def get_time_line_post():
+    return {
+        'timeline_posts': [
+            model_to_dict(p)
+            for p in TimelinePost.select().order_by(TimelinePost.created_at.desc())
+        ]
+    }
