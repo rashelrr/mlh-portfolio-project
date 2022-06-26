7cbca388f0f5cce3312eec3edea5f681eb2b3fd3
Rashel Rojas <rdr2139@columbia.edu>
2022-06-26 02:33:03 -0400

    Updated redeploy for service

--- redeploy-site.sh
+++ redeploy-site.sh
@@ -1,9 +1,8 @@
 #!/bin/bash
 
-tmux kill-session -t portfolio_session
 cd ~/mlh-portfolio-project
 git fetch && git reset origin/main --hard
 python -m venv python3-virtualenv
 source python3-virtualenv/bin/activate
 pip install -r requirements.txt
-tmux new-session -d -s portfolio_session 'flask run --host=0.0.0.0'
+systemctl restart myportfolio
