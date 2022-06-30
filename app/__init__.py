f2d6bef3aa9964ac6f2c78f59fee5bc8af57b5e3
Carlos Seda <carlosseda123@gmail.com>
2022-06-30 01:27:51 -0500

    flask testing

--- app/__init__.py
+++ app/__init__.py
@@ -1,19 +1,24 @@
 from peewee import *
 from playhouse.shortcuts import model_to_dict
 from flask import Flask, render_template, request
-from app.app import app
+from app.views import views
 import os
 import datetime
 
 x = Flask(__name__)
-x.register_blueprint(app)
+x.register_blueprint(views)
 
-mydb = MySQLDatabase(os.getenv("MYSQL_DATABASE"),
-                     user=os.getenv("MYSQL_USER"),
-                     password=os.getenv("MYSQL_PASSWORD"),
-                     host=os.getenv("MYSQL_HOST"),
-                     port=3306
-                    )
+if os.getenv("TESTING") == "true":
+    print("Running in test mode")
+    mydb = SqliteDatabase('file:memory?mode=memory&cache=shared', 
+    uri=True)
+else:
+    mydb = MySQLDatabase(os.getenv("MYSQL_DATABASE"),
+            user=os.getenv("MYSQL_USER"),
+            password=os.getenv("MYSQL_PASSWORD"),
+            host=os.getenv("MYSQL_HOST"),
+            port=3306
+        )
 
 class TimelinePost(Model):
     name = CharField()
